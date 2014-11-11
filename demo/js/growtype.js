0147becd8a299b2eb7abbfd12b87cdf732765f19
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-11 12:15:42 +0200

    Create growtype.js

---
+++ demo/js/growtype.js
@@ -0,0 +1,7 @@
+var w=window,
+    d=document,
+    e=d.documentElement,
+    g=d.getElementsByTagName('body')[0],
+    x=w.innerWidth||e.clientWidth||g.clientWidth;
+    
+g.style.fontSize = (x/200 + 12) + "px";
