7b9c303abb27690b5873e4a4bea2df11e713b8d7
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-12 00:27:04 +0200

    Update growtype.js

--- demo/js/growtype.js
+++ demo/js/growtype.js
@@ -1,14 +1,11 @@
-/*
-(function() {
-window.addEventListener('resize', fontSize);
-function fontSize(){
-    var w=window,
-        d=document,
-        e=d.documentElement,
-        g=d.getElementsByTagName("body")[0],
-        x=w.innerWidth||e.clientWidth||g.clientWidth;
-    g.style.fontSize = (x+4320)/5120 + "em";
-    g.style.lineHeight = 125e-6*x + 1.2;
-} 
-})();
-*/
+function typeScale(){
+  var a=window,
+      b=document,
+      c=b.documentElement,
+      d=b.getElementsByTagName("body")[0],
+      e=a.innerWidth||c.clientWidth||d.clientWidth;
+    d.style.fontSize=(e+4320)/5120+"em",
+    d.style.lineHeight=125e-6*e+1.2
+}
+window.addEventListener("resize",typeScale),
+typeScale();
