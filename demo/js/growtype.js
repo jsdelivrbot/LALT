f20ef24be080977b0974d7669725c9c084038035
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-11 14:58:13 +0200

    Update growtype.js

--- demo/js/growtype.js
+++ demo/js/growtype.js
@@ -1,11 +1,11 @@
-var w=window,
-    d=document,
-    e=d.documentElement,
-    g=d.getElementsByTagName("body")[0],
-    x=w.innerWidth||e.clientWidth||g.clientWidth;
 (function() {
 window.addEventListener('resize', fontSize);
 function fontSize(){
+    var w=window,
+        d=document,
+        e=d.documentElement,
+        g=d.getElementsByTagName("body")[0],
+        x=w.innerWidth||e.clientWidth||g.clientWidth;
     g.style.fontSize = (x+4320)/5120 + "em";
     g.style.lineHeight = 125e-6*x + 1.2;
 } 
