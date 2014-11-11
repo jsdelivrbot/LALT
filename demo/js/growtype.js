f002a6bbcfd6fd5e8b5dc929c04f09f059d59afc
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-11 13:46:23 +0200

    Update growtype.js

--- demo/js/growtype.js
+++ demo/js/growtype.js
@@ -3,12 +3,13 @@ var w=window,
     e=d.documentElement,
     g=d.getElementsByTagName("body")[0],
     x=w.innerWidth||e.clientWidth||g.clientWidth;
-    
-g.style.fontSize = (x+2400)/3200 + "em";
+g.style.fontSize = (x+4320)/5120 + "em";
 g.style.lineHeight = 125e-6*x + 1.2;
 
 /*
 size in pixels
+g.style.fontSize = x/320 + 13.5 + "px";
 
-g.style.fontSize = x/200 + 12 + "px";
+old equation
+g.style.fontSize = (x+2400)/3200 + "em";
 */
