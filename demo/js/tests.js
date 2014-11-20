8efeed45cf0286e7ec311dd671f9cfa79da62de6
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-20 20:50:29 +0200

    Update tests.js

--- demo/js/tests.js
+++ demo/js/tests.js
@@ -41,28 +41,3 @@ $LAB
 
 // write each inline initialization script in a seperate .wait function
 // these init. scripts are chained, they DON'T run simultaneously/asynchronously
-
-.wait(function(){
-	$.jaygrid();
-})
-.wait(function(){
-	$("#test-4").parallax("50%", 0.3);
-})
-.wait(function(){
-	$("table").stupidtable();
-})
-.wait(function(){
-	window.sr = new scrollReveal({
-		reset: true,
-		move: '222px',
-		mobile: true
-	});
-})
-.wait(function(){
-	visibly.onHidden(function (){
-    	$("body").css({"opacity":"0"})
-	}); 
-    visibly.onVisible(function (){
-    	$("body").velocity({ opacity: 1 }, { duration: 600 })
-	});
-}); // <-- remember to end with a semi-colon
