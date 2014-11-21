18cc862ba14b550debb90331e5e1f742a914f1a0
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-21 10:29:50 +0200

    Update alldone.js

--- demo/js/alldone.js
+++ demo/js/alldone.js
@@ -1,11 +1,10 @@
+var a = document.getElementsByTagName("body")[0];
 var c = document.getElementById("loader");
-window.onload = (function() {
-  c.className = "loaded";
-  a.className = a.className.replace("notdone","alldone");
-  setTimeout(function() {
-    a.removeChild(c);
-  }, 1500);
-});
+c.className = "loaded";
+a.className = a.className.replace("notdone","alldone");
+setTimeout(function() {
+  a.removeChild(c);
+}, 1500);
 window.onbeforeunload = (function() {
   a.id = "byebye";
 });
