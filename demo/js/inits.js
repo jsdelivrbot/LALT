de0bb243e1e24da1adc258da3d849ba23285134f
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-21 10:48:22 +0200

    Update inits.js

--- demo/js/inits.js
+++ demo/js/inits.js
@@ -1,17 +1,13 @@
 $LAB
-.script("js/john.js")
 .wait(function(){
 	$.jaygrid();
 })
-.script("js/paul.js")
 .wait(function(){
 	$("#test-4").parallax("50%", 0.3);
 })
-.script("js/george.js")
 .wait(function(){
 	$("table").stupidtable();
 })
-.script("js/ringo.js").wait()
 .wait(function(){
 	window.sr = new scrollReveal({
 		reset: true,
