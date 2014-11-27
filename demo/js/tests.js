40286963ab3d64487c130da46b9a111b970a2b85
paulbrowne <Paulmatthewbrowne@gmail.com>
2014-11-27 12:28:32 +0200

    Update tests.js

--- demo/js/tests.js
+++ demo/js/tests.js
@@ -42,5 +42,30 @@ $LAB
 	if ($('#test-4').length) {return "//cdnjs.cloudflare.com/ajax/libs/jquery-parallax/1.1.3/jquery-parallax-min.js"; }	
 	else {return null;}
 })
-.wait()
-.script("js/inits.js");
+.wait(function(){
+	hljs.initHighlighting();
+})
+.wait(function(){
+	$.jaygrid();
+})
+.wait(function(){
+	$("#test-4").parallax("50%", 0.3);
+})
+.wait(function(){
+	$("table").stupidtable();
+})
+.wait(function(){
+	window.sr = new scrollReveal({
+		reset: true,
+		move: '222px',
+		mobile: true
+	});
+})
+.wait(function(){
+	visibly.onHidden(function (){
+    	$("body").css({"opacity":"0"})
+	}); 
+    visibly.onVisible(function (){
+    	$("body").velocity({ opacity: 1 }, { duration: 600 })
+	});
+});
