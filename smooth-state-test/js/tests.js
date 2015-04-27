837e0a14b91a1327e0e6176bbdd2f5abecf5520c
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-04-27 14:58:10 +0300

    Update tests.js

--- smooth-state-test/js/tests.js
+++ smooth-state-test/js/tests.js
@@ -64,8 +64,7 @@ $LAB
 		prefetch: true,
 		pageCacheSize: 4,
 		callback : function(){
-			//$.getScript("https://rawgit.com/Paul-Browne/LALT/master/smooth-state-test/js/tests.js");
-			fetch("https://rawgit.com/Paul-Browne/LALT/master/smooth-state-test/js/tests.js");
+			$.getScript("https://rawgit.com/Paul-Browne/LALT/master/smooth-state-test/js/tests.js");
 		}
 	});
 })
