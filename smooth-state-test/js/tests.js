44cfb1c72b692a0f829b2c3ca2af7624da9ee9dc
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-05-26 15:28:09 +0300

    Update tests.js

--- smooth-state-test/js/tests.js
+++ smooth-state-test/js/tests.js
@@ -2,45 +2,45 @@ $LAB
 // write each test in a seperate .script function
 // all tests are run simultaneously/asynchronously
 .script(function(){
-	if ($('#main').length) {return ["js/jquery.smoothState.js", "js/smooth.css.js"]; }
+	if (document.getElementById("main").length) {return ["js/jquery.smoothState.js", "js/smooth.css.js"]; }
 	else {return null;}	
 })
 .script(function(){
 	
 	// load script if the the class *-col-* exists
 	
-	if ($('[class*="-col-"]').length) {return "//rawgit.com/Paul-Browne/jaygrid/master/js/jquery.jaygrid.min.js"; }
+	if (document.querySelectorAll('[class*="-col-"]').length) {return "//rawgit.com/Paul-Browne/jaygrid/master/js/jquery.jaygrid.min.js"; }
 	
 	// otherwise do nothing
 	
 	else {return null;}
 })
 .script(function(){
-	if ($('table').length) {return ["//cdnjs.cloudflare.com/ajax/libs/stupidtable/0.0.1/stupidtable.js", "js/table.css.js"]; }
+	if (document.getElementsByTagName('table').length) {return ["//cdnjs.cloudflare.com/ajax/libs/stupidtable/0.0.1/stupidtable.js", "js/table.css.js"]; }
 	else {return null;}	
 })
 .script(function(){
 	
 	// add stylesheets by including a .js file, that will inturn create a <link> with your stylesheets.
 	
-	if ($('.fontface [class*="icon-"]').length) {return "js/iconfont.css.js"; }
-	else if ($('.no-fontface [class*="icon-"]').length) {return "js/iconfont-fallback.css.js"; }
+	if (document.querySelectorAll('.fontface [class*="icon-"]').length) {return "js/iconfont.css.js"; }
+	else if (document.querySelectorAll('.no-fontface [class*="icon-"]').length) {return "js/iconfont-fallback.css.js"; }
 	else {return null;}
 })
 .script(function(){
-	if ($('.desktop').length) {return "//rawgit.com/addyosmani/visibly.js/master/visibly.js"; }
+	if (document.querySelectorAll('.desktop').length) {return "//rawgit.com/addyosmani/visibly.js/master/visibly.js"; }
 	else {return null;}
 })
 .script(function(){
-	if ($('[data-sr]').length) {return "js/scrollReveal.min.js"; }
+	if (document.querySelectorAll('[data-sr]').length) {return "js/scrollReveal.min.js"; }
 	else {return null;}
 })
 .script(function(){
-	if ($('pre code').length) {return ["js/highlight.js", "js/highlight.css.js"]; }
+	if (document.querySelectorAll('pre code').length) {return ["js/highlight.js", "js/highlight.css.js"]; }
 	else {return null;}
 })
 .script(function(){
-	if ($('#test-4').length) {return "//cdnjs.cloudflare.com/ajax/libs/jquery-parallax/1.1.3/jquery-parallax-min.js"; }	
+	if (document.getelementById('#test-4').length) {return "//cdnjs.cloudflare.com/ajax/libs/jquery-parallax/1.1.3/jquery-parallax-min.js"; }	
 	else {return null;}
 })
 
